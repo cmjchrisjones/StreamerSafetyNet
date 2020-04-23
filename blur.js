cba345872b9099fa57449aa2c1dca8d0875639dd
Chris Jones <cmj.chris.jones@googlemail.com>
2020-04-23 19:05:10 +0100

    Started trying to add Blur to the ebay username field, got background colour to work!

--- blur.js
+++ blur.js
@@ -1,9 +1,24 @@
 
+function blurSensitiveInfo(windowLocation){
+    switch (windowLocation) {
+        case "https://www.ebay.co.uk/":
+       
+            var mainPageUsernameCSSId = "gh-ug";
+            // document.getElementById(mainPageUsernameCSSId).style.filter = "blur('3px')";
+            // document.getElementById(mainPageUsernameCSSId).style.backgroundColor = "#765765";
+            var mainPageUsernameElement = document.getElementById(mainPageUsernameCSSId);
+            mainPageUsernameElement.style.backgroundColor = "#838383";
+            mainPageUsernameElement.setAttribute("style", "filter: blur('3px')");
+            break;
+    }
+}
+
 function getCurrentLocation() {
     var windowLocation = window.location.href;
 
     console.log(`Location ${windowLocation}`);
 
+    blurSensitiveInfo(windowLocation);
 }
 
 getCurrentLocation();
