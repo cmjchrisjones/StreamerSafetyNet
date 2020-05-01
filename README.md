9725aca5d031d32ee9b032677de3eb74da0463fc
Chris Jones <cmj.chris.jones@googlemail.com>
2020-05-01 02:06:18 +0100

    Added blur for ebay co.uk and com and amazon co.uk and com

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# StreamerSafetyNet
-A browser extension to keep streamers from doxxing themselves.
+# Welcome to StreamerSafetyNet 
+![](https://img.shields.io/static/v1?label=STATUS:&message=BETA&color=blue&style=for-the-badge)
+![Twitch Status](https://img.shields.io/twitch/status/theoriginaljerk?style=for-the-badge)
+
+- [Welcome to StreamerSafetyNet](#welcome-to-streamersafetynet)
+  - [What is it](#what-is-it)
+  - [RoadMap](#roadmap)
+  - [Installation](#installation)
+  - [Browser Support](#browser-support)
+
+## What is it
+
+A project designed to help prevent streamers from accidentally [doxxing](https://computer.howstuffworks.com/what-is-doxxing.htm) themselves whilst broadcasting. Will cover things up like email address, subscription IDs, API keys etc... If you have an idea for a site that we're not yet covering, feel free to add an issue to the repo.
+
+This was inspired by [Clarkio's AZ Mask](https://github.com/clarkio/azure-mask) project, but rather than limiting to Azure, making it useful for a whole myriad of websites that a streamer could potentially bring up on stream which might leak Personal Identifiable Information (PII).
+
+For the most part, I'm developing this mostly on live on [Twitch.tv]('https://twitch.tv/theoriginaljerk'), why not head over and follow my channel to see when I go live! Alternatively, when I am live the badge at the top will update!
+
+## RoadMap
+
+The aim is to cover a lot more sites, currently in this early beta release (v0.0.1), only ebay.co.uk, ebay.com, amazon.co.uk and amazon.com are supported. You can see more of our plans by looking at the [TODO](TODO.todo) file, if there is something not there that you think should be supported, feel free to open and/or vote for issues.
+
+## Installation
+
+Until the extension becomes available on the browser web stores, in the meantime, you can install the app by enabling developer mode and loading an unpacked extension.
+
+- Firefox: [https://extensionworkshop.com/documentation/develop/temporary-installation-in-firefox/](https://extensionworkshop.com/documentation/develop/temporary-installation-in-firefox/)
+- Chrome: [https://developer.chrome.com/extensions/getstarted](https://developer.chrome.com/extensions/getstarted#manifest)
+- Edge [https://docs.microsoft.com/en-us/microsoft-edge/extensions-chromium/getting-started/part1-simple-extension#run-your-extension-locally-in-your-browser-while-developing-it-side-loading](https://docs.microsoft.com/en-us/microsoft-edge/extensions-chromium/getting-started/part1-simple-extension#run-your-extension-locally-in-your-browser-while-developing-it-side-loading)
+  
+## Browser Support
+
+|OS|Browser|Status|
+|:-:|:-:|:-:|
+|Windows|Edge (Chromium) | ✔ |
+|Windows| Chrome | ✔ |
+|Windows| Firefox | ✔ |
+|Mac OS| Safari | ⚠ |
+|Mac OS| Firefox | ⚠ |
+|Mac OS| Chrome | ⚠ |
+|Mac OS| Edge | ⚠ |
+|Linux| Firefox | ⚠ |
+|Linux| Chrome | ⚠ |
+|Linux| Edge | ⚠ |
+
+| | |
+|:-|:-|
+|✔|Tested successfully|
+|⚠|Untested|
+|❌|Doesn't work / Not supported|
--- blur.js
+++ blur.js
@@ -1,24 +1,94 @@
-
-function blurSensitiveInfo(windowLocation){
-    switch (windowLocation) {
-        case "https://www.ebay.co.uk/":
-       
-            var mainPageUsernameCSSId = "gh-ug";
-            // document.getElementById(mainPageUsernameCSSId).style.filter = "blur('3px')";
-            // document.getElementById(mainPageUsernameCSSId).style.backgroundColor = "#765765";
-            var mainPageUsernameElement = document.getElementById(mainPageUsernameCSSId);
-            mainPageUsernameElement.style.backgroundColor = "#838383";
-            mainPageUsernameElement.setAttribute("style", "filter: blur('3px')");
-            break;
+var ebayIdsToBlur = ['gh-ug', 'gh-ui', 'gh-un', 'gh-uid', 'userid', 'user-info', 'pass'];
+var ebayClassesToBlur = ['mbg-id'];
+var amazonIdsToBlur = ['glow-toaster-body', 'authportal-center-section', 'glow-ingress-block', 'GLUXAddressBlock', 'ap_email'];
+var amazonClassesToBlur = ['cvf-widget-container', 'auth-text-truncate', 'recipient-address', 'displayAddressDiv', 'normal-desktop-address-tile']
+function applyBlurToElementById(id) {
+    if (id !== undefined) {
+        var element = document.getElementById(id);
+        if (element != null) {
+            element.style.webkitFilter = "blur(3px)";
+        }
     }
 }
 
 function getCurrentLocation() {
     var windowLocation = window.location.href;
-
-    console.log(`Location ${windowLocation}`);
-
-    blurSensitiveInfo(windowLocation);
+    blurTheThing(null, windowLocation);
 }
 
+const observer = new MutationObserver(mutations => {
+    mutations.filter(m => {
+        blurTheThing(window.location.href, m);
+    });
+});
+
+const config = {
+    attributes: false,
+    characterData: true,
+    childList: true,
+    subtree: true
+};
+observer.observe(document.body, config);
+
 getCurrentLocation();
+
+function blurTheThing(site, m) {
+    if (site === null) {
+        return;
+    }
+    var site = site.includes('ebay') ? 'ebay' :
+        site.includes('amazon') ? 'amazon' : undefined;
+
+    switch (site) {
+        case 'ebay':
+            ebayIdsToBlur.map(eid => {
+                applyBlurToElementById(eid);
+            });
+            //https://medium.com/@chuckdries/traversing-the-dom-with-filter-map-and-arrow-functions-1417d326d2bc
+
+            ebayClassesToBlur.map(ec => {
+                var childs = document.getElementsByClassName(ec);
+                childs = Array.from(childs);
+                child = childs.map(e => {
+                    e.style.webkitFilter = "blur(3px)";
+
+                });
+            });
+            if (m != null || m != undefined) {
+                // if m is null then this might be first page load
+                if (m.target['baseURI'].includes('ebay.co.uk')) {
+                    ebayIdsToBlur.map(eid => {
+                        if (m.target['id'] != "" && eid === m.target['id']) {
+                            applyBlurToElementById(eid);
+                        }
+                    });
+                }
+            }
+            break;
+        case 'amazon':
+            amazonIdsToBlur.map(eid => {
+                applyBlurToElementById(eid);
+            });
+            amazonClassesToBlur.map(ec => {
+                var childs = document.getElementsByClassName(ec);
+                childs = Array.from(childs);
+                child = childs.map(e => {
+                    e.style.webkitFilter = "blur(3px)";
+
+                });
+            });
+            // if m is null then this might be first page load
+            if (m != null || m != undefined) {
+                if (m.target['baseURI'].includes('amazon.co.uk')) {
+                    amazonIdsToBlur.map(eid => {
+                        if (m.target['id'] != "" && eid === m.target['id']) {
+                            applyBlurToElementById(eid);
+                        }
+                    });
+                }
+            }
+            break;
+        default:
+            return;
+    }
+}
